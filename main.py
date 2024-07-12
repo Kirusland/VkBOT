92b77ccf79fce96d52629ca1cf5147483d7cd7cd
Kirill Matytsyn <73121729+Kirusland@users.noreply.github.com>
2024-07-12 22:40:09 +0300

    Update main.py
    Почти доделал команду !игнор и добавил команду !чс

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import func
+import db
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.exceptions import *
 from datetime import datetime
@@ -9,21 +10,16 @@ for event in VkLongPoll(func.sessiongroup).listen():
         func.set_user_status(f"VK API Кодер❤️ Последний заход в сеть: {datetime.now()}")
         text = event.text.lower()
         if text[0] == '!':
+
             peer_id = event.peer_id
-
-            if peer_id > 2000000000:
-                chat_id = peer_id - 2000000000
-
-            else:
-                chat_id = peer_id
-
             user_id = event.user_id
             message_id = event.message_id
+            chat_id = event.chat_id
 
-            print(chat_id)
+            print(peer_id)
             print(user_id)
             print(message_id)
-            print(peer_id)
+            print(chat_id)
 
             if ' ' in text:
                 prefix, arg = text.split(' ', 1)
@@ -34,60 +30,81 @@ for event in VkLongPoll(func.sessiongroup).listen():
                     if (prefix == '!реши'):
                         try:
                             msg = eval(arg)
-                            func.send_message_by_peer_id(msg, chat_id)
+                            func.send_message_by_peer_id(msg, peer_id)
                         except ZeroDivisionError:
-                            func.send_message_by_peer_id("Нельзя делить на ноль!", chat_id)
+                            func.send_message_by_peer_id("Нельзя делить на ноль!", peer_id)
+                            continue
+                        except NameError:
+                            func.send_message_by_peer_id("Произошла ошибка! Проверьте правильность написания решаемного выражения", peer_id)
                             continue
                     else:
                         func.send_message_by_peer_id(f"Аргумент команды не является числом, напишите: !help",
-                                                     chat_id)
+                                                     peer_id)
                     continue
 
 
                 else:
-                    fname = func.get_fname_by_id(arg, 'dat')
-                    lname = func.get_lname_by_id(arg, 'dat')
+
 
                     if prefix == '!друзья':
-                        func.add_friend_by_id(arg, chat_id)
+                        func.add_friend_by_id(arg, peer_id)
+
+                    elif prefix == '!игнор':
+                        fname = func.get_fname_by_id(arg, 'gen')
+                        lname = func.get_lname_by_id(arg, 'gen')
+                        if db.check_user_id(arg) == 0:
+                            db.write_user_id(arg)
+                            func.send_message_by_peer_id(f"Добавил {lname} {fname} в игнор", peer_id)
+                        else:
+                            func.send_message_by_peer_id(f"Уже игнорирую {lname} {fname}", peer_id)
+                        print(db.all_user_ids)
 
                     elif prefix == '!инфо':
                         # info = func.get_profie_info(arg)
                         func.send_message_by_peer_id(f"Команда недоступна из-за политики VK API:"
                                                      f"\n(vk_api.exceptions.ApiError: [914] Message is too long)",
-                                                     chat_id)
+                                                     peer_id)
                         #vk_api.exceptions.ApiError: [914] Message is too long
                     elif prefix == '!статус':
                         status = func.get_user_status(arg)
-                        func.send_message_by_peer_id(status, chat_id)
+                        func.send_message_by_peer_id(status, peer_id)
 
                     # elif prefix == '!добавить':
-                    #     func.add_user_by_id(chat_id, arg)
+                    #     func.add_user_by_id(peer_id, arg)
 
-                    # elif prefix == 'игнор':
-                    #     добавление в .txt или в .db id пользователя
-                    #     реализация проверки наличия пользователя в файле
+
+                    elif prefix == '!чс':
+                        fname = func.get_fname_by_id(arg, 'acc')
+                        lname = func.get_lname_by_id(arg, 'acc')
+                        try:
+                            func.ban_user_by_id(arg)
+                            func.send_message_by_peer_id(f"Добавил {lname} {fname} в черный список", peer_id)
+                        except ApiError:
+                            func.send_message_by_peer_id(f"Удалил {lname} {fname} из  черного списка", peer_id)
+                            func.unban_user_by_id(arg)
+
+
 
                     else:
                         func.send_message_by_peer_id(f"Команда не найдена, чтобы вывести список команд, напишите: !help",
-                                                     chat_id)
+                                                     peer_id)
 
             else:
                 if text == '!статус':
                     status = func.get_my_status()
-                    func.send_message_by_peer_id(status, chat_id)
+                    func.send_message_by_peer_id(status, peer_id)
 
                 elif text == '!онлайн':
                     online_friends = func.get_friends_online()
                     if online_friends == []:
-                        func.send_message_by_peer_id(f"Никого нет онлайн", chat_id)
+                        func.send_message_by_peer_id(f"Никого нет онлайн", peer_id)
                     else:
-                        func.send_message_by_peer_id(f"Онлайн: {online_friends}", chat_id)
+                        func.send_message_by_peer_id(f"Онлайн: {online_friends}", peer_id)
                     print(online_friends)
 
                 elif text == '!ping' or text == '!пинг':
-                    ping=f'Пинг: {abs(round(datetime.now().timestamp()-time_, 3))} сек.'
-                    func.send_message_by_peer_id(ping, chat_id)
+                    ping=f'Пoнг: {abs(round(datetime.now().timestamp()-time_, 3))} сек.'
+                    func.send_message_by_peer_id(ping, peer_id)
                     print(ping)
 
                 elif text == '!помощь' or text == '!help':
@@ -100,6 +117,6 @@ for event in VkLongPoll(func.sessiongroup).listen():
                         f"\n!помощь или !help - вывести это сообщение"
                         f"\n!пинг или !ping - проверить пинг"
                         f"\n!реши - решить пример",
-                        chat_id)
+                        peer_id)
                 else:
-                    func.send_message_by_peer_id(f"Команда не найдена. Чтобы вывести список команд, напишите: !help", chat_id)
+                    func.send_message_by_peer_id(f"Команда не найдена. Чтобы вывести список команд, напишите: !help", peer_id)
